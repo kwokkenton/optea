789ab3598f1353d9b1dc2b1f514c8bc3d0649a9c
Srayan Gangopadhyay <sgango@users.noreply.github.com>
2023-03-03 17:46:12 +0000

    attempt fitting gaussian to in-focus mtfs

--- mtf/mtf_analysis.py
+++ mtf/mtf_analysis.py
@@ -3,23 +3,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.fft import fft, fftfreq
 import cv2
+from scipy.optimize import curve_fit
 
+# gaussian function and initial guess borrowed from Kenton's code
+def gaussian(x, sigma):
+    return np.exp(-x**2/(2*sigma**2))
 
 def mtf_ft_plot(freqs, MTF, in_focus, f_number):
     t_freqs = freqs[: len(freqs) // 2]
     t_MTF = MTF[in_focus][: len(MTF[in_focus]) // 2]
 
     fig, ax = plt.subplots(1, 2, figsize=(10, 5))
-    ax[0].plot(t_freqs, t_MTF)
+    ax[0].plot(t_freqs, t_MTF, label='data')
     ax[0].set_title(f"in-focus MTF at f/{f_number}")
     ax[0].set_xlabel("line pairs per millimetre")
+    
+    popt, pcov = curve_fit(gaussian, t_freqs, t_MTF, p0=[23])
+    ax[0].plot(t_freqs, gaussian(t_freqs, *popt), 'r-', label=f'Gaussian fit, sigma={popt[0]}')
+    ax[0].legend()
 
     N = len(t_freqs)
     T = np.mean(np.diff(t_freqs))
+    # T = 0.25 # NOTE: what difference does this make?
 
-    yf = fft(t_MTF)
+    yf_data = fft(t_MTF)
+    yf_fit = fft(gaussian(t_freqs, *popt))
     xf = fftfreq(N, T)
-    ax[1].plot(xf * 1000, 2.0 / N * np.abs(yf), ".")
+    ax[1].plot(xf * 1000, 2.0 / N * np.abs(yf_data), "bx", label='FT{data}')
+    ax[1].plot(xf * 1000, 2.0 / N * np.abs(yf_fit), "rx", label='FT{fit}')
+    ax[1].legend()
     ax[1].set_title("FT(MTF)")
     ax[1].set_xlim(-100, 100)
     ax[1].set_xlabel("distance (microns)")
